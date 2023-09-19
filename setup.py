d8f550934da5a0125bfafa66cb651c7c00a7f0b2
Christian B. Mendl <christian.b.mendl@gmail.com>
2023-09-19 09:19:21 +0200

    specifying minimum required Python version 3.10

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email="christian.b.mendl@gmail.com",
     packages=["fh_comm"],
     url="https://github.com/qc-tum/fermi_hubbard_commutators",
+    python_requires=">=3.10",
     install_requires=[
         "numpy",
         "scipy",
