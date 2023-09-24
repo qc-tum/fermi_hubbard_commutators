e08ef5ffb79e8c45ea49ab1f084ab5057181863a
Christian B. Mendl <christian.b.mendl@gmail.com>
2023-09-24 11:09:49 +0200

    exploring effect of "s" in commutator bound demo

--- examples/comm_bound_demo.py
+++ examples/comm_bound_demo.py
@@ -31,6 +31,18 @@ def main():
     terms = fhc.commutator_bound(rule, s)
     for term in terms:
         print(term)
+    terms_ref = terms
+    print()
+
+    # Strang splitting rule with two Hamiltonian terms, non-optimal 's'
+    rule = fhc.SplittingMethod.suzuki(2, 1)
+    print(rule)
+    s = 1
+    print("s:", s, "(non-optimal)")
+    terms = fhc.commutator_bound(rule, s)
+    for term, term_ref in zip(terms, terms_ref):
+        assert term.commidx == term_ref.commidx
+        print(term, "diff:", term.weight - term_ref.weight)
     print()
 
     # Strang splitting rule with three Hamiltonian terms
@@ -56,6 +68,40 @@ def main():
     terms = fhc.commutator_bound(rule, s)
     for term in terms:
         print(term)
+    terms_ref = terms
+    print()
+
+    # fourth-order product rule with two Hamiltonian terms, 's' not centered
+    rule = fhc.SplittingMethod.suzuki(2, 2)
+    print(rule)
+    s = (rule.num_layers + 1) // 2 - 1
+    print("s:", s, "(not centered)")
+    terms = fhc.commutator_bound(rule, s)
+    for term, term_ref in zip(terms, terms_ref):
+        assert term.commidx == term_ref.commidx
+        print(term, "diff:", term.weight - term_ref.weight)
+    print()
+
+    # fourth-order product rule with two Hamiltonian terms, 's' not centered
+    rule = fhc.SplittingMethod.suzuki(2, 2)
+    print(rule)
+    s = (rule.num_layers + 1) // 2 + 1
+    print("s:", s, "(not centered)")
+    terms = fhc.commutator_bound(rule, s)
+    for term, term_ref in zip(terms, terms_ref):
+        assert term.commidx == term_ref.commidx
+        print(term, "diff:", term.weight - term_ref.weight)
+    print()
+
+    # fourth-order product rule with two Hamiltonian terms, 's' not centered
+    rule = fhc.SplittingMethod.suzuki(2, 2)
+    print(rule)
+    s = (rule.num_layers + 1) // 2 + 2
+    print("s:", s, "(not centered)")
+    terms = fhc.commutator_bound(rule, s)
+    for term, term_ref in zip(terms, terms_ref):
+        assert term.commidx == term_ref.commidx
+        print(term, "diff:", term.weight - term_ref.weight)
     print()
 
     # fourth-order product rule with three Hamiltonian terms
@@ -66,6 +112,18 @@ def main():
     terms = fhc.commutator_bound(rule, s)
     for term in terms:
         print(term)
+    terms_ref = terms
+    print()
+
+    # fourth-order product rule with three Hamiltonian terms, non-optimal 's'
+    rule = fhc.SplittingMethod.suzuki(3, 2)
+    print(rule)
+    s = (rule.num_layers + 1) // 2 - 1
+    print("s:", s, "(non-optimal)")
+    terms = fhc.commutator_bound(rule, s)
+    for term, term_ref in zip(terms, terms_ref):
+        assert term.commidx == term_ref.commidx
+        print(term, "diff:", term.weight - term_ref.weight)
     print()
 
 
